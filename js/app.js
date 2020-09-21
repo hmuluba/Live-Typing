bd9adca178f08421064bf61de5df3bf83f26b6cc
Mulher <hmuluba@gmail.com>
2020-09-21 16:00:20 +0100

    suppression d'une ligne

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 const textarea = document.querySelector('textarea');
 const result = document.querySelector('.result');
-const msg = "Entrain d'ecire";
 
 textarea.addEventListener('keyup', function() {
     result.innerHTML = textarea.value;
