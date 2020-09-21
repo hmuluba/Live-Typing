5347bb381c6a4b91036cc36c8e516f1061b958e2
Mulher <hmuluba@gmail.com>
2020-09-21 15:46:26 +0100

    modification fichier js

--- js/app.js
+++ js/app.js
@@ -5,11 +5,3 @@ const msg = "Entrain d'ecire";
 textarea.addEventListener('keyup', function() {
     result.innerHTML = textarea.value;
 } );
-
-
-const bouton = document.querySelector('.btn');
-
-
-bouton.addEventListener("mouseover", function () {
-   alert(Math.random());
-})
